6581fbb7d0512c22c224691f068b6c924d9c35ea
vignesh <vigneshwar.mani@chronus.com>
2014-03-05 16:47:59 +0530

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Myapp::Application.routes.draw do
+  get "static_pages/home"
+
+  get "static_pages/help"
+  
+  get "static_pages/about"
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
